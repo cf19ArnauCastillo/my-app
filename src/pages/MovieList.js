b225cf17693f929706002aaa2bb525d8e57ac9c6
Cristian Montañés <cmontaes@everis.com>
2022-05-20 12:29:21 +0200

    Acabado

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -3,18 +3,22 @@ import "./MovieList.css";
 
 const MovieList = () => {
   return (
-      peliculas.forEach((item) => {
-        <body>
-        <input id="titol" type="text" placeholder="Titol" className="bigItem"></input><br></br><br></br>
-        <input id="desc" type="text" placeholder="Descripció" className="bigItem"></input><br></br><br></br>
-        <input id="dir" type="text" placeholder="Direcció" className="bigItem"></input><br></br><br></br>
-        <input id="img" type="text" placeholder="Imatge (url)" className="bigItem"></input><br></br><br></br>
-        <input id="nota" type="text" placeholder="Nota (1-5)" className="littleItem"></input>
-        <input id="any" type="text" placeholder="Any" className="littleItem"></input>
-        <input id="durada" type="text" placeholder="Durada" className="littleItem"></input><br></br><br></br>
-      </body>
-      })
-    )
+    <div>
+      {peliculas.map((item) => {
+        const list = (
+          <>
+          <div className='container'>
+            <img src={item.imatge} className='imagen' />
+            <div className="bottom-left">{item.nota}</div>
+            <div className='bottom-centered-top'>{item.titol}</div>
+            <div className='bottom-centered'>{item.direccio}</div>
+          </div>
+          </>
+        );
+      return list;
+    })}
+    </div>
+    );
 };
 
 export default MovieList;
